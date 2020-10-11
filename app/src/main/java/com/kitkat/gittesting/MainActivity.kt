74cdf14fb81f3be0744f796f4f6705ef0a6d7b4a
loveleenkaurnancy <loveleen.nancy@gmail.com>
2020-10-11 21:18:12 +0530

    Initial Commit

---
+++ app/src/main/java/com/kitkat/gittesting/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kitkat.gittesting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
